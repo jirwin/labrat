ec1fd423b57be5f5876325a402240af7b297fb0d
Justin Gallardo <justin.gallardo@gmail.com>
2015-07-26 20:54:30 -0700

    refactor: Break logic out into smaller functions. Support functions for enabled, as well as a percentage.

--- index.js
+++ index.js
@@ -2,13 +2,38 @@ var async = require('async');
 var deepEqual = require('deep-equal');
 var uuid = require('node-uuid');
 
+
+/**
+ * Returns a labrat function.
+ * @param {String} name The name of the experiment.
+ * @param {Function} control The control function.
+ * @param {Function} candidate The candidate function.
+ * @param {Writable} publishStream A Writable object stream to publish results to.
+ * @param {Object} options An options object.
+ * @returns {Function}
+ */
+var LabRat = function(name, control, candidate, publishStream, options) {
+  this.name = name;
+  this.control = control;
+  this.candidate = candidate;
+  this.publishStream = publishStream;
+  this.options = options;
+
+  if (this.publishStream && !this.publishStream.writable && !this.publishStream._writeableState) {
+    this.options = publishStream;
+    this.publishStream = null;
+  }
+
+  this.options = this.options || {};
+};
+
 /**
  * Takes an asynchronous function and reports the amount of time it takes to run, as well as values it returns.
  * @param func The function to observe.
  * @param args The arguments to call the function with.
  * @returns {Function} The function to call.
  */
-var observeFunction = function(func, args) {
+LabRat.prototype.observeFunction = function(func, args) {
   return function(callback) {
     var now = Date.now();
     func.apply(null, args.concat(function() {
@@ -21,21 +46,41 @@ var observeFunction = function(func, args) {
 };
 
 /**
- * Returns a labrat function.
- * @param {String} name The name of the experiment.
- * @param {Function} control The control function.
- * @param {Function} candidate The candidate function.
- * @param {Writable} publishStream A Writable object stream to publish results to.
- * @param {Object} options An options object.
- * @returns {Function}
+ * Publish observations to the provided publish stream.
+ * @param observations
  */
-module.exports = function(name, control, candidate, publishStream, options) {
-  if (publishStream && !publishStream.writable && !publishStream._writeableState) {
-    options = publishStream;
-    publishStream = null;
+LabRat.prototype.publish = function(observations) {
+  if (this.publishStream) {
+    this.publishStream.write(observations);
+  }
+};
+
+/**
+ * Returns true if the candidate should be run.
+ * @return {Boolean}
+ */
+LabRat.prototype.isEnabled = function() {
+  var enabled = this.options.enabled;
+
+  if (enabled === undefined || enabled === null) {
+    enabled = true;
   }
 
-  options = options || {};
+  if (typeof enabled === 'function') {
+    console.log('Checking if enabled', enabled());
+    return enabled();
+  }
+
+  if (typeof enabled === 'number') {
+    return enabled > Math.random() * 100;
+
+  }
+
+  return enabled;
+};
+
+LabRat.prototype.get = function() {
+  var self = this;
 
   return function() {
     var args, cb;
@@ -43,19 +88,19 @@ module.exports = function(name, control, candidate, publishStream, options) {
     args = Array.prototype.slice.call(arguments);
     cb = args.pop();
     async.auto({
-      control: observeFunction(control, args),
+      control: self.observeFunction(self.control, args),
       candidate: ['control', function(callback) {
-        if (options.enabled === false) {
-          callback(null, {});
+        if (self.isEnabled()) {
+          self.observeFunction(self.candidate, args)(callback);
         } else {
-          observeFunction(candidate, args)(callback);
+          callback(null, {});
         }
       }]
     }, function(err, results) {
       var observations = {
         control: results.control,
         candidate: results.candidate,
-        name: name,
+        name: self.name,
         id: uuid.v1(),
         mismatch: false
       };
@@ -64,13 +109,17 @@ module.exports = function(name, control, candidate, publishStream, options) {
         observations.mismatch = !deepEqual(observations.control.values, observations.candidate.values);
       }
 
-      // If a publishStream is provided, write the observations to it
-      if (publishStream) {
-        publishStream.write(observations);
-      }
+      self.publish(observations);
 
       cb.apply(null, observations.control.values);
     });
   };
+};
+
+
+module.exports = function(name, control, candidate, publishStream, options) {
+  var labrat = new LabRat(name, control, candidate, publishStream, options);
+
+  return labrat.get();
 };
 
--- test/test-main.js
+++ test/test-main.js
@@ -94,3 +94,254 @@ test('disabled experiment', function(t) {
     t.end();
   });
 });
+
+test('enabled experiment with function', function(t) {
+  var oneRan = false,
+    twoRan = false,
+    publishStream = through2.obj(),
+    options = {},
+    run;
+
+  function one(val, callback) {
+    oneRan = Date.now();
+    setTimeout(function() {
+      callback(null, val, 1 + val);
+    }, 100);
+  }
+
+  function two(val, callback) {
+    twoRan = Date.now();
+    setTimeout(function() {
+      callback(null, val, 2 + val);
+    }, 200);
+  }
+
+  publishStream.on('data', function(obj) {
+    t.ok(obj, 'A results object was returned');
+    t.equal(obj.name, 'test', 'The experiment name was properly set.');
+    t.ok(obj.hasOwnProperty('id'), 'The experiment was assigned an id');
+    t.ok(obj.control, 'A control observation was returned');
+    t.ok(obj.control.hasOwnProperty('duration'), 'The control observation has a duration');
+    t.ok(obj.control.duration >= 100, 'The control duration took at least 5 seconds');
+    t.deepEqual(obj.control.values, [null, 3, 4], 'The control observation has the expected results');
+    t.ok(obj.candidate, 'A candidate observation was returned');
+    t.ok(obj.candidate.hasOwnProperty('duration'), 'The candidate observation has a duration');
+    t.ok(obj.candidate.duration >= 200, 'The candidate duration took at least 2 seconds');
+    t.deepEqual(obj.candidate.values, [null, 3, 5], 'The candidate observation has the expected results');
+    t.equal(obj.mismatch, true, 'The results have mismatched values as expected.');
+  });
+
+  options.enabled = function() {
+    return true;
+  };
+
+  run = labrat('test', one, two, publishStream, options);
+
+  run(3, function(err, res1, res2) {
+    t.error(err, 'No error while running.');
+    t.ok(oneRan, 'Function one ran.');
+    t.ok(twoRan, 'Function two ran.');
+    t.ok(oneRan < twoRan, 'Function one ran before function two.');
+    t.equal(res1, 3, 'The resulting function returned 3 as its second argument.');
+    t.equal(res2, 4, 'The resulting function returned 4 as its third argument.');
+    t.end();
+  });
+});
+
+test('enabled experiment with boolean', function(t) {
+  var oneRan = false,
+    twoRan = false,
+    publishStream = through2.obj(),
+    options = {},
+    run;
+
+  function one(val, callback) {
+    oneRan = Date.now();
+    setTimeout(function() {
+      callback(null, val, 1 + val);
+    }, 100);
+  }
+
+  function two(val, callback) {
+    twoRan = Date.now();
+    setTimeout(function() {
+      callback(null, val, 2 + val);
+    }, 200);
+  }
+
+  publishStream.on('data', function(obj) {
+    t.ok(obj, 'A results object was returned');
+    t.equal(obj.name, 'test', 'The experiment name was properly set.');
+    t.ok(obj.hasOwnProperty('id'), 'The experiment was assigned an id');
+    t.ok(obj.control, 'A control observation was returned');
+    t.ok(obj.control.hasOwnProperty('duration'), 'The control observation has a duration');
+    t.ok(obj.control.duration >= 100, 'The control duration took at least 5 seconds');
+    t.deepEqual(obj.control.values, [null, 3, 4], 'The control observation has the expected results');
+    t.ok(obj.candidate, 'A candidate observation was returned');
+    t.ok(obj.candidate.hasOwnProperty('duration'), 'The candidate observation has a duration');
+    t.ok(obj.candidate.duration >= 200, 'The candidate duration took at least 2 seconds');
+    t.deepEqual(obj.candidate.values, [null, 3, 5], 'The candidate observation has the expected results');
+    t.equal(obj.mismatch, true, 'The results have mismatched values as expected.');
+  });
+
+  options.enabled = true;
+
+  run = labrat('test', one, two, publishStream, options);
+
+  run(3, function(err, res1, res2) {
+    t.error(err, 'No error while running.');
+    t.ok(oneRan, 'Function one ran.');
+    t.ok(twoRan, 'Function two ran.');
+    t.ok(oneRan < twoRan, 'Function one ran before function two.');
+    t.equal(res1, 3, 'The resulting function returned 3 as its second argument.');
+    t.equal(res2, 4, 'The resulting function returned 4 as its third argument.');
+    t.end();
+  });
+});
+
+test('enabled experiment with percentage', function(t) {
+  var oneRan = false,
+    twoRan = false,
+    publishStream = through2.obj(),
+    options = {},
+    run;
+
+  function one(val, callback) {
+    oneRan = Date.now();
+    setTimeout(function() {
+      callback(null, val, 1 + val);
+    }, 100);
+  }
+
+  function two(val, callback) {
+    twoRan = Date.now();
+    setTimeout(function() {
+      callback(null, val, 2 + val);
+    }, 200);
+  }
+
+  publishStream.on('data', function(obj) {
+    t.ok(obj, 'A results object was returned');
+    t.equal(obj.name, 'test', 'The experiment name was properly set.');
+    t.ok(obj.hasOwnProperty('id'), 'The experiment was assigned an id');
+    t.ok(obj.control, 'A control observation was returned');
+    t.ok(obj.control.hasOwnProperty('duration'), 'The control observation has a duration');
+    t.ok(obj.control.duration >= 100, 'The control duration took at least 5 seconds');
+    t.deepEqual(obj.control.values, [null, 3, 4], 'The control observation has the expected results');
+    t.ok(obj.candidate, 'A candidate observation was returned');
+    t.ok(obj.candidate.hasOwnProperty('duration'), 'The candidate observation has a duration');
+    t.ok(obj.candidate.duration >= 200, 'The candidate duration took at least 2 seconds');
+    t.deepEqual(obj.candidate.values, [null, 3, 5], 'The candidate observation has the expected results');
+    t.equal(obj.mismatch, true, 'The results have mismatched values as expected.');
+  });
+
+  options.enabled = 100;
+
+  run = labrat('test', one, two, publishStream, options);
+
+  run(3, function(err, res1, res2) {
+    t.error(err, 'No error while running.');
+    t.ok(oneRan, 'Function one ran.');
+    t.ok(twoRan, 'Function two ran.');
+    t.ok(oneRan < twoRan, 'Function one ran before function two.');
+    t.equal(res1, 3, 'The resulting function returned 3 as its second argument.');
+    t.equal(res2, 4, 'The resulting function returned 4 as its third argument.');
+    t.end();
+  });
+});
+
+test('disabled experiment with function', function(t) {
+  var oneRan = false,
+    twoRan = false,
+    publishStream = through2.obj(),
+    options = {},
+    run;
+
+  function one(val, callback) {
+    oneRan = Date.now();
+    setTimeout(function() {
+      callback(null, val, 1 + val);
+    }, 100);
+  }
+
+  function two(val, callback) {
+    twoRan = Date.now();
+    setTimeout(function() {
+      callback(null, val, 2 + val);
+    }, 200);
+  }
+
+  publishStream.on('data', function(obj) {
+    t.ok(obj, 'A results object was returned');
+    t.equal(obj.name, 'test', 'The experiment name was properly set.');
+    t.ok(obj.hasOwnProperty('id'), 'The experiment was assigned an id');
+    t.ok(obj.control, 'A control observation was returned');
+    t.ok(obj.control.hasOwnProperty('duration'), 'The control observation has a duration');
+    t.ok(obj.control.duration >= 100, 'The control duration took at least 5 seconds');
+    t.deepEqual(obj.control.values, [null, 3, 4], 'The control observation has the expected results');
+    t.deepEqual(obj.candidate, {}, 'An empty candidate observation was returned.');
+    t.equal(obj.mismatch, false, 'The results have mismatched values as expected.');
+  });
+
+  options.enabled = function() {
+    return false;
+  };
+
+  run = labrat('test', one, two, publishStream, options);
+
+  run(3, function(err, res1, res2) {
+    t.error(err, 'No error while running.');
+    t.ok(oneRan, 'Function one ran.');
+    t.notOk(twoRan, 'Function two did not run.');
+    t.equal(res1, 3, 'The resulting function returned 3 as its second argument.');
+    t.equal(res2, 4, 'The resulting function returned 4 as its third argument.');
+    t.end();
+  });
+});
+
+test('disabled experiment with percentage', function(t) {
+  var oneRan = false,
+    twoRan = false,
+    publishStream = through2.obj(),
+    options = {},
+    run;
+
+  function one(val, callback) {
+    oneRan = Date.now();
+    setTimeout(function() {
+      callback(null, val, 1 + val);
+    }, 100);
+  }
+
+  function two(val, callback) {
+    twoRan = Date.now();
+    setTimeout(function() {
+      callback(null, val, 2 + val);
+    }, 200);
+  }
+
+  publishStream.on('data', function(obj) {
+    t.ok(obj, 'A results object was returned');
+    t.equal(obj.name, 'test', 'The experiment name was properly set.');
+    t.ok(obj.hasOwnProperty('id'), 'The experiment was assigned an id');
+    t.ok(obj.control, 'A control observation was returned');
+    t.ok(obj.control.hasOwnProperty('duration'), 'The control observation has a duration');
+    t.ok(obj.control.duration >= 100, 'The control duration took at least 5 seconds');
+    t.deepEqual(obj.control.values, [null, 3, 4], 'The control observation has the expected results');
+    t.deepEqual(obj.candidate, {}, 'An empty candidate observation was returned.');
+    t.equal(obj.mismatch, false, 'The results have mismatched values as expected.');
+  });
+
+  options.enabled = 0;
+
+  run = labrat('test', one, two, publishStream, options);
+
+  run(3, function(err, res1, res2) {
+    t.error(err, 'No error while running.');
+    t.ok(oneRan, 'Function one ran.');
+    t.notOk(twoRan, 'Function two did not run.');
+    t.equal(res1, 3, 'The resulting function returned 3 as its second argument.');
+    t.equal(res2, 4, 'The resulting function returned 4 as its third argument.');
+    t.end();
+  });
+});
